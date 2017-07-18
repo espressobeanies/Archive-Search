861a7f2d51245176f3be7b501f4b0489156923c4
Dave Florek <espressobeanies@gmail.com>
2017-07-18 14:39:57 -0400

    Update archive-search.sh

--- archive-search.sh
+++ archive-search.sh
@@ -2,16 +2,22 @@
 yum -y install dialog
 echo "Archive Searcher"
 
-###"Dialog syntax -> Single-line, calendar
+###Declare variables
 SRCHSTRG=$(dialog --title "Enter search string" --inputbox  1 16);
 SAVEAS=$(dialog --title "Output save location" --fselect /~ 14 48);
 STRTDATE=$(dialog --title "Start Date" --calendar); 
 ENDDATE=$(dialog --title "End Date" --calendar);
 
-###Searcher using zgrep and awk
-for(i=0;i<31;i++)
+###Declare functions
+search()
 {
-zgrep "$SRCHSTRG" /home/BroLogs/* >> $SAVEAS &
+  for(i=$1;i<=$2;i++)
+    {
+      zgrep "$SRCHSTRG" /home/BroLogs/$i* >> $SAVEAS &    
+    }
 }
+
+###Execute functions
+search $STRTDY $ENDDY
 
 ###Output
