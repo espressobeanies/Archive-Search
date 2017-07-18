e98419a4f20fb26d3fb2e3e75df09f37c45ffc43
DF <espressobeanies@gmail.com>
2017-07-18 10:24:19 -0400

    Update archive-search.sh

--- archive-search.sh
+++ archive-search.sh
@@ -4,7 +4,14 @@ echo "Archive Searcher"
 
 ###"Dialog syntax -> Single-line, calendar
 SRCHSTRG=$(dialog --title "Enter search string" --inputbox  1 16);
-SAVEAS=$(dialog --title "text" --fselect /~ 14 48);
+SAVEAS=$(dialog --title "Output save location" --fselect /~ 14 48);
+STRTDATE=$(dialog --title "Start Date" --calendar); 
+ENDDATE=$(dialog --title "End Date" --calendar);
+
 ###Searcher using zgrep and awk
+for(i=0;i<31;i++)
+{
+zgrep /home/BroLogs/* | awk{'print $1 $3 $4'} >> $SAVEAS
+}
 
 ###Output
